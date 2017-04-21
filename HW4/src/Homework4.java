cdb37f3b9809aa818fcdc8c5ea7a0bf1cb77a2b7
Vladyslove <od.vladyslav@gmail.com>
2017-04-21 14:11:53 +0300

    First task 'variables' was done

--- HW4/src/Homework4.java
+++ HW4/src/Homework4.java
@@ -1,5 +1,12 @@
 public class Homework4 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+      int tni = 1;
+      float taolf = 10.21234f;
+      String gnirtS = "Assalam aleikum";
+
+        System.out.println(tni);
+        System.out.println(taolf);
+        System.out.println(gnirtS);
     }
 }
