a4ef1e58985b765f4b4995a6f14e629aff16bbe9
Vladyslove <od.vladyslav@gmail.com>
2017-04-21 15:57:58 +0300

    Second part of HW4 -Class method(function) creating and calling

--- HW4/src/Homework4.java
+++ HW4/src/Homework4.java
@@ -1,12 +1,32 @@
 public class Homework4 {
-    public static void main(String[] args) {
 
-      int tni = 1;
-      float taolf = 10.21234f;
-      String gnirtS = "Assalam aleikum";
 
-        System.out.println(tni);
-        System.out.println(taolf);
-        System.out.println(gnirtS);
+      public static void main(String[] args) {
+
+      // First part of HW4 - Variables
+
+      // int tni = 1;
+      // float taolf = 10.21234f;
+      // String gnirtS = "Assalam aleikum";
+      //   System.out.println(tni);
+      //   System.out.println(taolf);
+      //   System.out.println(gnirtS);
+
+      //Second part of HW4 -Class method(function) creating and calling
+        // Item A
+
+        System.out.print("hello ");
+        System.out.print("hello ");
+        System.out.print("hello ");
+        System.out.print("hello ");
+        System.out.print("hello ");
+
+        // Item B
+        System.out.println("");
+
+        for (int i = 1; i <=5 ; i++ ) {
+            System.out.print("hello ");
+        }
+
     }
 }
